bdcb50385cb8ab60f24e67e82e0df61155ba6c56
Kishan S Patel <bishan.batel@protonmail.com>
2024-05-02 18:51:59 -0500

    103 current  2024-05-02 18:51:54  24.05.20240427.58a1abd   6.6.28                          *

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,31 +1,33 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    gh
     git-credential-oauth
+    gh
   ];
 
-  programs.gh-dash = {
-    enable = true;
-    # settings = {};
-    catppuccin.enable = true;
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "Kishan S Patel";
-    userEmail = "bishan.batel@protonmail.com";
-
-    extraConfig = {
-      url = {
-        "https://github.com/" = {
-          insteadOf = "gh:";
-        };
-      };
-
-      init.defaultBranch = "main";
-      credential.helper = "${pkgs.gh}/bin/gh auth git-credential";
+  programs = {
+    gh-dash = {
+      enable = true;
+      # settings = {};
+      catppuccin.enable = true;
     };
 
-    aliases = {};
+    git = {
+      enable = true;
+      userName = "Kishan S Patel";
+      userEmail = "bishan.batel@protonmail.com";
+
+      extraConfig = {
+        url = {
+          "https://github.com/" = {
+            insteadOf = "gh:";
+          };
+        };
+
+        init.defaultBranch = "main";
+        credential.helper = "${pkgs.gh}/bin/gh auth git-credential";
+      };
+
+      aliases = {};
+    };
   };
 }
