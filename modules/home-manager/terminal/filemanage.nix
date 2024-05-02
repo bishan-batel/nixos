c93e587cedfbd29f3452f0c8b4142f209d9e131b
Kishan S Patel <bishan.batel@protonmail.com>
2024-05-02 18:38:52 -0500

    96 current  2024-05-02 18:38:48  24.05.20240427.58a1abd   6.6.28                          *

--- modules/home-manager/terminal/filemanage.nix
+++ modules/home-manager/terminal/filemanage.nix
@@ -1,6 +1,12 @@
 {pkgs, ...}: {
   programs.yazi = {
     enable = true;
+    enableFishIntegration = true;
+
+    settings = {
+      show_hidden = true;
+    };
+
     catppuccin.enable = true;
   };
 }
