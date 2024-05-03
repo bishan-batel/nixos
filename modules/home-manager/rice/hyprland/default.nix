0cba300d425bf4d4187c9d493669d2550991c5eb
Kishan S Patel <bishan.batel@protonmail.com>
2024-05-03 15:12:41 -0500

    121 current  2024-05-03 15:12:35  24.05.20240427.58a1abd   6.6.28                          *

--- modules/home-manager/rice/hyprland/default.nix
+++ modules/home-manager/rice/hyprland/default.nix
@@ -8,6 +8,6 @@
   ];
 
   home.file."~/.config/hypr/hyprland.conf" = {
-    source = config.lib.file.mkOutOfStoreSymlink ./hyprland.conf;
+    source = config.lib.file.mkOutOfStoreSymlink nixos/modules/home-manager/rice/hyprland/hyprland.conf;
   };
 }
