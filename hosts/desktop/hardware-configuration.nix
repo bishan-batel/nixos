3b0c4a41bf30ff24de01c4bb036d91dcb6c96ee7
Kishan S Patel <bishan.batel@protonmail.com>
2024-06-26 18:41:24 -0500

    Generation 256: 2024-06-26T23:41:14Z

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -16,10 +16,15 @@
   boot.initrd.kernelModules = [
     "v4l2loopback"
   ];
+
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = with config.boot.kernelPackages; [
     v4l2loopback
   ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
 
   boot.kernel.sysctl = {
     "kernel.perf_event_paranoid" = -1;
