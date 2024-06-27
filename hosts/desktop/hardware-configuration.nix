6744c7f689ea6739ca69fd22c498d811fed19efc
Kishan S Patel <bishan.batel@protonmail.com>
2024-06-27 15:43:02 -0500

    Generation 262: 2024-06-27T20:42:52Z

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -13,15 +13,10 @@
   ];
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [
-    "v4l2loopback"
-  ];
+  boot.kernelModules = ["kvm-amd" "v4l2loopback"];
 
-  boot.kernelModules = ["kvm-amd"];
+  boot.extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
 
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
-  ];
   boot.extraModprobeConfig = ''
     options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
   '';
