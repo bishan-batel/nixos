a465bbe4205013dbd0f78000aaf75ed817bb919c
Kishan S Patel <bishan.batel@protonmail.com>
2024-07-16 18:46:40 -0500

    Generation 283: 2024-07-16T23:46:29Z

--- modules/home-manager/rice/waybar/default.nix
+++ modules/home-manager/rice/waybar/default.nix
@@ -1,112 +1,73 @@
 {
+  osConfig,
   config,
-  pkgs,
   lib,
-  inputs,
+  pkgs,
   ...
-}: {
+}: let
+  # Bar v1
+  # waybar_config = import ./og/config.nix {inherit osConfig config lib pkgs;};
+  # waybar_style = import ./og/style.nix {inherit (config) colorscheme;};
+  # NixBar
+  # waybar_config = import ./nixbar/config.nix {inherit osConfig config lib pkgs;};
+  # waybar_style = import ./nixbar/style.nix {inherit (config) colorscheme;};
+  # Tokyonight
+  # waybar_config = import ./tokyonight/config.nix {inherit osConfig config lib pkgs;};
+  # waybar_style = import ./tokyonight/style.nix {inherit (config) colorscheme;};
+  # Catppuccin
+  # waybar_config = import ./catppuccin/config.nix {inherit osConfig config lib pkgs;};
+  # waybar_style = import ./catppuccin/style.nix {inherit (config) colorscheme;};
+  # Simple bar
+  waybar_config = import ./simple/config.nix {inherit osConfig config lib pkgs;};
+  waybar_style = import ./simple/style.nix {inherit (config) colorscheme;};
+in {
   programs.waybar = {
     enable = true;
-    # package = inputs.hyprland.packages.${pkgs.system}.waybar-hyprland;
-    settings = {
-      mainBar = {
-        layer = "top";
-        position = "top";
-        modules-left = [
-          # "wlr/workspaces"
-          "hyprland/window"
-        ];
-        modules-center = [];
-        modules-right = [
-          "pulseaudio"
-          "network"
-          "backlight"
-          #"battery"
-          "clock"
-          "tray"
-          #"custom/power"
-        ];
-
-        # "wlr/workspaces" = {
-        #   disable-scroll = true;
-        #   sort-by-name = true;
-        #   format = "{icon}";
-        #   format-icons = { default = ""; };
-        # };
-
-        "wlr/workspaces" = {
-          disable-scroll = true;
-          sort-by-name = true;
-          format = "{icon}";
-          format-icons = {default = "";};
-        };
-
-        pulseaudio = {
-          format = " {icon} ";
-          format-muted = "ﱝ";
-          format-icons = ["奄" "奔" "墳"];
-          tooltip = true;
-          tooltip-format = "{volume}%";
-        };
-
-        network = {
-          format-wifi = " ";
-          format-disconnected = "睊";
-          format-ethernet = " ";
-          tooltip = true;
-          tooltip-format = "{signalStrength}%";
-        };
-
-        backlight = {
-          device = "intel_backlight";
-          format = "{icon}";
-          format-icons = ["" "" "" "" "" "" "" "" ""];
-          tooltip = true;
-          tooltip-format = "{percent}%";
-        };
-
-        battery = {
-          states = {
-            warning = 30;
-            critical = 15;
-          };
-          format = "{icon}";
-          format-charging = "";
-          format-plugged = "";
-          format-icons = ["" "" "" "" "" "" "" "" "" "" "" ""];
-          tooltip = true;
-          tooltip-format = "{capacity}%";
-        };
-
-        "custom/power" = {
-          tooltip = false;
-          on-click = "powermenu";
-          format = "襤";
-        };
-
-        clock = {
-          tooltip-format = ''
-            <big>{:%Y %B}</big>
-            <tt><small>{calendar}</small></tt>'';
-          format-alt = ''
-             {:%d
-             %m
-            %Y}'';
-          format = ''
-            {:%H
-            %M}'';
-        };
-
-        tray = {
-          icon-size = 21;
-          spacing = 10;
-        };
-      };
-    };
-
-    style = builtins.readFile ./style.css;
+    package = pkgs.waybar;
+    settings = waybar_config;
+    style = waybar_style;
   };
+  xdg = {
+    configFile = {
+      "waybar/machiatto.css".text = ''
+        /*
+        *
+        * Catppuccin Macchiato palette
+        * Maintainer: rubyowo
+        *
+        */
 
-  xdg.configFile."waybar/mocha.css".text =
-    builtins.readFile ./mocha.css;
+        @define-color base   #24273a;
+        @define-color mantle #1e2030;
+        @define-color crust  #181926;
+
+        @define-color text     #cad3f5;
+        @define-color subtext0 #a5adcb;
+        @define-color subtext1 #b8c0e0;
+
+        @define-color surface0 #363a4f;
+        @define-color surface1 #494d64;
+        @define-color surface2 #5b6078;
+
+        @define-color overlay0 #6e738d;
+        @define-color overlay1 #8087a2;
+        @define-color overlay2 #939ab7;
+
+        @define-color blue      #8aadf4;
+        @define-color lavender  #b7bdf8;
+        @define-color sapphire  #7dc4e4;
+        @define-color sky       #91d7e3;
+        @define-color teal      #8bd5ca;
+        @define-color green     #a6da95;
+        @define-color yellow    #eed49f;
+        @define-color peach     #f5a97f;
+        @define-color maroon    #ee99a0;
+        @define-color red       #ed8796;
+        @define-color mauve     #c6a0f6;
+        @define-color pink      #f5bde6;
+        @define-color flamingo  #f0c6c6;
+        @define-color rosewater #f4dbd6;
+      '';
+    };
+  };
 }
