74cc12e70d3220ac73f0b84ab8886ede425be14e
Kishan S Patel <bishan.batel@protonmail.com>
2024-07-31 21:11:28 -0500

    Generation 341: 2024-08-01T02:11:14Z

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -56,7 +56,7 @@
     isNormalUser = true;
     description = "Kishan S Patel";
     extraGroups = ["networkmanager" "wheel"];
-    packages = with pkgs; [gcc clang linuxKernel.packages.linux_zen.perf xwaylandvideobridge];
+    packages = with pkgs; [gcc clang linuxKernel.packages.linux_zen.perf xwaylandvideobridge vesktop];
   };
 
   nix.settings = {
