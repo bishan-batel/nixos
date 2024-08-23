68ce3bedadb04cbd456bed2bbaad60521dcb4aec
Kishan S Patel <bishan.batel@protonmail.com>
2024-08-22 23:15:43 -0500

    Generation 365: 2024-08-23T04:15:28Z

--- modules/home-manager/editors.nix
+++ modules/home-manager/editors.nix
@@ -4,18 +4,9 @@
   config,
   ...
 }: {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    vimAlias = true;
-    viAlias = true;
-  };
-
   home = {
     packages = with pkgs; [
       jetbrains-toolbox
-      neovide
-      wl-clipboard
     ];
   };
 }
