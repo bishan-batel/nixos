36d574e03576999df61968e59aa0710aea377557
Kishan S Patel <bishan.batel@protonmail.com>
2024-08-25 19:08:13 -0500

    Generation 14: 2024-08-26T00:07:58Z

--- modules/home/scaling/default.nix
+++ modules/home/scaling/default.nix
@@ -8,11 +8,11 @@
     '';
 
   wayland.windowManager.hyprland.settings = {
-    env = {
-      "QT_SCALE_FACTOR" = 2;
-      "GDK_SCALE" = 2;
-      "QT_AUTO_SCREEN_SCALE_FACTOR" = 1;
-    };
+    env = [
+      "QT_SCALE_FACTOR, 2"
+      "GDK_SCALE, 2"
+      "QT_AUTO_SCREEN_SCALE_FACTOR, 2"
+    ];
 
     xwayland = {
       force_zero_scaling = true;
