d21ea29903b0a17a953d83be634f0f267d2c365b
Kishan S Patel <bishan.batel@protonmail.com>
2024-08-25 19:16:21 -0500

    Generation 18: 2024-08-26T00:16:16Z

---
+++ modules/auto-upgrade.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "--no-write-lock-file"
+      "-L"
+    ];
+    dates = "06:00";
+    randomizedDelaySec = "45min";
+  };
+}
