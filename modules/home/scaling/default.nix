416aab3517538cee41e1314b4d5979506a686dac
Kishan S Patel <bishan.batel@protonmail.com>
2024-08-25 19:49:08 -0500

    Generation 21: 2024-08-26T00:49:01Z

--- modules/home/scaling/default.nix
+++ modules/home/scaling/default.nix
@@ -8,12 +8,12 @@
     '';
 
   wayland.windowManager.hyprland.settings = {
-    env = [
-      "QT_SCALE_FACTOR, 2"
-      "GDK_SCALE, 2"
-      "QT_AUTO_SCREEN_SCALE_FACTOR, 2"
-      "AQ_DRM_DEVICES,/dev/dri/card1:/dev/dri/card2"
-    ];
+    env = {
+      "QT_SCALE_FACTOR" = 2;
+      "GDK_SCALE" = 2;
+      "QT_AUTO_SCREEN_SCALE_FACTOR" = 2;
+      "AQ_DRM_DEVICES" = "/dev/dri/card1 =/dev/dri/card2";
+    };
 
     xwayland = {
       force_zero_scaling = true;
