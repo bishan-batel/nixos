685743c7956767c9b03ddca2f8a53214ab4f68bf
Kishan S Patel <bishan.batel@protonmail.com>
2024-09-03 04:41:22 +0000

    Generation 390: 2024-09-03T04:41:10Z

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -34,7 +34,8 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/LosAngeles";
+  # time.timeZone = "America/LosAngeles";
+  services.automatic-timezoned.enable = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
