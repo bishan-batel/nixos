ac72d88147ed618d007c99d6e6f767aa31122090
Kishan S Patel <bishan.batel@protonmail.com>
2024-09-13 18:29:17 -0700

    Generation 87: 2024-09-14T01:28:54Z

--- modules/home/terminal/tmux.nix
+++ modules/home/terminal/tmux.nix
@@ -4,6 +4,30 @@
     shell = "${pkgs.nushell}/bin/nu";
     terminal = "tmux-256color";
     historyLimit = 1000;
-    # plugins = with pkgs; [ ];
+    newSession = true;
+    escapeTime = 0;
+    secureSocket = false;
+
+    plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
+    ];
+
+    extraConfig = ''
+      # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
+      set -g default-terminal "xterm-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+      set-environment -g COLORTERM "truecolor"
+
+      unbind-key -a
+      set-option -g prefix C-space
+
+      # Mouse works as expected
+      set-option -g mouse on
+      # easy-to-remember split pane commands
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
+    '';
   };
 }
