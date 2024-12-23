a220bf229279ccb65f4177720bd67237541e0a6e
Kishan S Patel <bishan.batel@protonmail.com>
2024-12-23 07:55:12 -0800

    Generation 143: 2024-12-23T15:55:04Z

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -24,6 +24,9 @@
 
     extraModulePackages = with pkgs; [linuxPackages.v4l2loopback];
 
+    kernel.sysctl."vm.max_map_count" = 2147483642;
+    blacklistedKernelModules = ["nouveau"];
+
     extraModprobeConfig = ''
       options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
     '';
