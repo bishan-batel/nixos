798f4db995ee31c6307a8efa39c92fd90b22163f
Kishan S Patel <bishan.batel@protonmail.com>
2025-06-09 16:03:49 -0700

    Generation 569: 2025-06-09T23:03:46Z

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -182,11 +182,20 @@
 
   xdg.portal = {
     enable = true;
+    xdgOpenUsePortal = true;
+
     extraPortals = [
-      # pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal-wlr
-      # pkgs.xwaylandvideobridge
+      pkgs.xdg-desktop-portal-gtk
     ];
+
+    config = {
+      common.default = "*";
+      hyprland = {
+        default = ["gtk" "hyprland"];
+      };
+    };
   };
 
   # Steam
