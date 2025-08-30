5da23c26832591bf7a28513901ffc1da68122591
Kishan S Patel <bishan.batel@protonmail.com>
2025-08-29 23:00:53 -0700

    Fixed obsidian performance issues

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -56,7 +56,7 @@
     extraGroups = ["networkmanager" "wheel" "disk"];
     packages = with pkgs; [
       linuxKernel.packages.linux_zen.perf
-      vesktop
+      legcord
       # xwaylandvideobridge
       acpi
       wtype
@@ -192,7 +192,8 @@
     enable = true;
     gamescopeSession.enable = false;
   };
-  programs.gamemode.enable = false;
+
+  programs.gamemode.enable = true;
 
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATH = "\${HOME}/.steam/root/compatabilitytools.d";
@@ -201,22 +202,6 @@
   # Power management
   services = {
     thermald.enable = true;
-
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
-      };
-    };
 
     auto-cpufreq = {
       enable = true;
--- modules/home/utilities.nix
+++ modules/home/utilities.nix
@@ -2,6 +2,8 @@
   home.packages = with pkgs; [
     pavucontrol
     gimp3-with-plugins
+    krita
+    inkscape
     thunderbird
     quickemu
 
