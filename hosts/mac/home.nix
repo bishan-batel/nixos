e10536d5f245abd49267e6fecc1145e611f29ca9
Kishan S Patel <bishan.batel@protonmail.com>
2025-12-08 00:03:14 -0800

    Added cursor trail & fixed darwin linkapps

--- hosts/mac/home.nix
+++ hosts/mac/home.nix
@@ -28,23 +28,25 @@
     homeDirectory = "/Users/bishan_";
   };
 
-  home.file."Applications/home-manager".source = let
-    apps = pkgs.buildEnv {
-      name = "home-manager-applications";
-      paths = config.home.packages;
-      pathsToLink = "/Applications";
-    };
-  in "${apps}/Applications";
+  # home.file."Applications/home-manager".source = let
+  #   apps = pkgs.buildEnv {
+  #     name = "home-manager-applications";
+  #     paths = config.home.packages;
+  #     pathsToLink = "/Applications";
+  #   };
+  # in "${apps}/Applications";
 
-  home.activation = {
-    aliasApplications = lib.hm.dag.entryAfter ["writeBoundary"] ''
-           app_folder=$(echo ~/Applications);
-           for app in $(find "$genProfilePath/home-path/Applications" -type l); do
-      $DRY_RUN_CMD rm -f $app_folder/$(basename $app)
-      $DRY_RUN_CMD osascript -e "tell app \"Finder\"" -e "make new alias file at POSIX file \"$app_folder\" to POSIX file \"$app\"" -e "set name of result to \"$(basename $app)\"" -e "end tell"
-           done
-    '';
-  };
+  targets.darwin.linkApps.enable = true;
+
+  # home.activation = {
+  #   aliasApplications = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  #          app_folder=$(echo ~/Applications);
+  #          for app in $(find "$genProfilePath/home-path/Applications" -type l); do
+  #     $DRY_RUN_CMD rm -f $app_folder/$(basename $app)
+  #     $DRY_RUN_CMD osascript -e "tell app \"Finder\"" -e "make new alias file at POSIX file \"$app_folder\" to POSIX file \"$app\"" -e "set name of result to \"$(basename $app)\"" -e "end tell"
+  #          done
+  #   '';
+  # };
 
   catppuccin = {
     enable = true;
--- modules/home/terminal/kitty.nix
+++ modules/home/terminal/kitty.nix
@@ -8,9 +8,12 @@
       confirm_os_window_close = 0;
       copy_on_select = true;
       clipboard_control = "write-clipboard read-clipboard write-primary";
-      cursor_trail = 0;
       font_size = 15;
       cursor_shape = "block";
+      cursor_trail = 100;
+      cursor_trail_enabled = true;
+      cursor_trail_start_threshold = 0;
+      cursor_trail_decay = 0.5;
     };
   };
 }
