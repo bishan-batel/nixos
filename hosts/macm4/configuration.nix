2b1b7b6efbfdbffd078862af05faeca41e1b9b80
Kishan S Patel <bishan.batel@protonmail.com>
2026-02-21 00:30:34 -0800

    Fixed lix settings

--- hosts/macm4/configuration.nix
+++ hosts/macm4/configuration.nix
@@ -16,6 +16,7 @@
 
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
+  nix.package = pkgs.lixPackageSets.stable.lix;
 
   # Set Git commit hash for darwin-version.
   # system.configurationRevision = self.rev or self.dirtyRev or null;
