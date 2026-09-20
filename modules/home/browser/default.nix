497aa1c89396abaeafd0c274c34e82cccd7b7d64
Kishan S Patel <bishan.batel@protonmail.com>
2026-09-20 14:56:08 -0700

    updates

--- modules/home/browser/default.nix
+++ modules/home/browser/default.nix
@@ -1,3 +1,7 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [brave firefox];
+{pkgs, inputs, ...}: {
+  home.packages = with pkgs; [
+    brave 
+    firefox
+    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
 }
