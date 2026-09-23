cf06d35d33704e835cb00958d257fe099925b108
Kishan S Patel <bishan.batel@protonmail.com>
2026-09-23 10:23:33 -0700

    fixed zen

--- modules/home/browser/default.nix
+++ modules/home/browser/default.nix
@@ -1,15 +1,29 @@
-{pkgs, inputs, ...}: {
+{ pkgs, inputs, ... }:
+{
 
   imports = [
-    inputs.zen-browser.homeModules.beta
+    inputs.zen-browser.homeModules.default
     # or inputs.zen-browser.homeModules.twilight
     # or inputs.zen-browser.homeModules.twilight-official
+    # inputs.zen-browser.packages."${pkgs.system}".beta
   ];
 
-  home.packages = with pkgs; [brave firefox];
+  home.packages = with pkgs; [
+    brave
+    firefox
+  ];
 
   programs.zen-browser = {
-    enable = true;
-    setAsDefaultBrowser = true;
+    # enabl = true;
+    # setAsDefaultBrowser = true;
+
+    profiles.default.presets.catppuccin = {
+      enable = true;
+      flavor = "Mocha"; # Frappe | Latte | Macchiato | Mocha
+      accent = "Mauve"; # Blue, Flamingo, Green, Lavender, Maroon, Mauve, ...
+    };
+
+    darwinDefaultsId = "app.zen-browser.zen.plist";
+
   };
 }
